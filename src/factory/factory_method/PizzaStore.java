4e74dd3168a8733e5a1d6075a198c84ea1db6c26
ylpitseleh <lyla02217@gmail.com>
2023-03-05 23:55:40 +0900

    팩토리 패턴

--- src/factory/factory_method/PizzaStore.java
+++ src/factory/factory_method/PizzaStore.java
@@ -3,9 +3,11 @@ package factory.factory_method;
 abstract class PizzaStore {
     abstract Pizza createPizza(String item);
 
-    Pizza orderPizza(String type) {
-        Pizza pizza = createPizza(type);
+    Pizza orderPizza(String type) { // 팩토리 메소드 패턴이 특화된 템플릿 메소드 패턴이라고 불리는 이유
+        Pizza pizza = createPizza(type); // 생성을 캡슐화
+
         System.out.println("--- Making a " + pizza.getName() + " ---");
+        // 템플릿 메소드처럼 알고리즘을 캡슐화
         pizza.prepare();
         pizza.bake();
         pizza.cut();
